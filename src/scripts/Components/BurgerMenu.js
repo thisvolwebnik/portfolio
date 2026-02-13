f713f61a4aee3c1c8985f512ca97d7fd651153f2
Nikita(thisvolwebnik) <thisvolwebnik@yandex.ru>
2026-02-13 13:13:29 +0300

    feat: add a pinned header, navigation links, and scrolling functionality to a selected section, closing the burger menu

--- src/scripts/Components/BurgerMenu.js
+++ src/scripts/Components/BurgerMenu.js
@@ -3,6 +3,7 @@ class BurgerMenu {
     root: "[data-js-header]",
     overlay: "[data-js-header-overlay]",
     burgerButton: "[data-js-header-burger-button]",
+    linkMenu: "[data-js-header-link]",
   };
 
   stateClasses = {
@@ -12,25 +13,31 @@ class BurgerMenu {
 
   constructor() {
     this.rootElement = document.querySelector(this.selectors.root);
-    this.overlayElement = this.rootElement.querySelector(
-      this.selectors.overlay,
-    );
-    this.burgerButtonElement = this.rootElement.querySelector(
-      this.selectors.burgerButton,
-    );
+    this.overlayElement = this.rootElement.querySelector(this.selectors.overlay);
+    this.burgerButtonElement = this.rootElement.querySelector(this.selectors.burgerButton);
+    this.linksMenuElement = this.rootElement.querySelectorAll(this.selectors.linkMenu);
     this.bindEvents();
   }
 
-  onClickBurgerButton = () => {
+  toggleClassesAllElements() {
     this.burgerButtonElement.classList.toggle(this.stateClasses.isActive);
     this.overlayElement.classList.toggle(this.stateClasses.isActive);
     document.documentElement.classList.toggle(this.stateClasses.isLook);
+  }
+
+  onClickBurgerButton = () => {
+    this.toggleClassesAllElements();
+  };
+
+  onClickLinkElement = () => {
+    this.toggleClassesAllElements();
   };
 
   bindEvents() {
-    this.burgerButtonElement.addEventListener(
-      "click",
-      this.onClickBurgerButton,
+    this.burgerButtonElement.addEventListener("click", this.onClickBurgerButton);
+
+    this.linksMenuElement.forEach(linkElement =>
+      linkElement.addEventListener("click", this.onClickLinkElement)
     );
   }
 }
